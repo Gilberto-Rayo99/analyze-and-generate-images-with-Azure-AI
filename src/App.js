051df6e013e3feafa0d94c40b6ffb40fecc59dfd
Rayo <jg.rayo999@gmail.com>
2023-11-29 15:43:19 -0600

    returnint APP.JS

--- src/App.js
+++ src/App.js
@@ -1,29 +1,8 @@
 import React from 'react';
 
 function App() {
-
-  return (
-    <>
-      <div style={{ flex: 1 }}>
-        <h1 type="title" style={{ margin: 20 }}>Computer Vision</h1>
-
-        <div style="flex-direction:column">
-          <p>Insert URL or type prompt:</p>
-
-          <input type="text" placeholder='ENTER URL o analye prompt to generate an image'></input>
-
-          <div style={{ "flex-direction": "row", margin: 20 }}>
-            <button >
-              Analyze
-            </button>
-            <button>
-              Generate
-            </button>
-          </div>
-        </div>
-      </div>
-    </>
-  )
+  const value = 'World';
+  return <div>Hello {value}</div>;
 }
 
 export default App;
