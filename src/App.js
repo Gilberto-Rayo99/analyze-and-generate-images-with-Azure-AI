f11251fcc4371a66593c6a391b4b7d5e39bfded7
Rayo <jg.rayo999@gmail.com>
2023-11-29 15:41:01 -0600

    Changing App.js html

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+
+  return (
+    <>
+      <div style={{ flex: 1 }}>
+        <h1 type="title" style={{ margin: 20 }}>Computer Vision</h1>
+
+        <div style="flex-direction:column">
+          <p>Insert URL or type prompt:</p>
+
+          <input type="text" placeholder='ENTER URL o analye prompt to generate an image'></input>
+
+          <div style={{ "flex-direction": "row", margin: 20 }}>
+            <button >
+              Analyze
+            </button>
+            <button>
+              Generate
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default App;
